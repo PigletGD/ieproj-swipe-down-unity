85f473d3dcaafce9f94497313d4a985678e11113
WarcramSpartanix <warrenlucenara@gmail.com>
2021-06-28 16:55:25 +0800

    Changed UI assets for the Shop/Tabs

--- sWipe Down/Assets/Scripts/UI/TabManager.cs
+++ sWipe Down/Assets/Scripts/UI/TabManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -11,15 +12,15 @@ public class TabManager : MonoBehaviour
 
     public Tab lastActiveTab;
 
-    [SerializeField]private bool active = true;
-    [SerializeField]private Vector2 toggleOnPostion;
-    [SerializeField]private Vector2 toggleOffPostion;
+    [ReadOnly]private bool active = true;
+    [ReadOnly]private Vector2 toggleOnPostion;
+    [ReadOnly] private Vector2 toggleOffPostion;
 
     // Start is called before the first frame update
     private void Start()
     {
         toggleOnPostion = buttonsHolder.transform.position;
-        toggleOffPostion = buttonsHolder.transform.position;
+        toggleOffPostion = toggleOnPostion;
         toggleOffPostion.y -= Tab.TAB_SIZE;
     }
 
@@ -27,8 +28,8 @@ public class TabManager : MonoBehaviour
     {
         float lerp;
 
-        if (active) lerp = Mathf.Lerp(buttonsHolder.anchoredPosition.y, 0, 0.1f);
-        else lerp = Mathf.Lerp(buttonsHolder.anchoredPosition.y, -Tab.TAB_SIZE, 0.1f);
+        if (active) lerp = Mathf.Lerp(buttonsHolder.anchoredPosition.y, 35, 0.1f);
+        else lerp = Mathf.Lerp(buttonsHolder.anchoredPosition.y, -Tab.TAB_SIZE + 35, 0.1f);
 
         //if (active) lerp = Mathf.Lerp(buttonsHolder.position.y, toggleOnPostion.y, 0.1f);
         //else lerp = Mathf.Lerp(buttonsHolder.position.y, toggleOffPostion.y, 0.1f);
