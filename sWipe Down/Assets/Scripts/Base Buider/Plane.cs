e4e6d1057c0434240d15fabe8b092dca0ef1d151
Jacob Manzano <jacobmanzano2@gmail.com>
2021-07-11 19:55:53 +0800

    Script Arrangement

--- sWipe Down/Assets/Scripts/Base Buider/Plane.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WhyWontYouDelete {
-    // IGNORE THIS CLASS, ITS BREAKING UNITY COLLAB FOR ME...
-}
