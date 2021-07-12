91774abb3a2483c32220a269dfefa9aaa4e8c425
JasoSantiago <68273034+JasoSantiago@users.noreply.github.com>
2021-07-12 13:41:17 +0800

    added tpgenerator start

---
+++ sWipe Down/Assets/Scripts/Base Buider/Tower Behaviour/TPGenerationBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPGenerationBehaviour : TowerBehaviour
+{
+    [SerializeField] private GameManager manager;
+    private int basegain = 10;
+    public override void ExecuteAction()
+    {
+        manager.IncrementScore(basegain);
+    }
+
+    public override bool ReadyToExecuteAction()
+    {
+        if (time >= fireRate) return true;
+        return false;
+    }
+}
